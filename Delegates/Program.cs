2063dacbeaaa0e7d79a00f6eff6e87860ec42b0b
Bruna Reveriego <Bruna.Reveriego@br.ey.com>
2021-11-22 17:57:01 -0300

    utilizando metodos anonimos

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -11,29 +11,41 @@ namespace Delegates
 
         public delegate int MetodoDelegate(string Nome);
 
-        public static int Qualquer(string Pessoa)
-        {
-            Console.WriteLine("Seu Nome: " + Pessoa);
-            Console.ReadKey();
-            return Pessoa.Length;
-        }
+        //public static int Qualquer(string Pessoa)
+        //{
+        //    Console.WriteLine("Seu Nome: " + Pessoa);
+        //    Console.ReadKey();
+        //    return Pessoa.Length;
+        //}
 
         public static void UtilizaDelegate(MetodoDelegate metodoDelegate)
         {
             var letras = metodoDelegate("Maria");
             Console.WriteLine("Quantidade de letras: " + letras);
+            
         }
 
 
 
         static void Main(string[] args)
         {
+            /*Utilizando métodos anonimos */
 
-            UtilizaDelegate(Qualquer);
+            UtilizaDelegate(delegate (string n) {
+                Console.WriteLine("Nome: " + n);
+                return n.Length;
+            });
 
-            MetodoDelegate metodoDelegate = Qualquer;
+            MetodoDelegate metodoDelegate = delegate(string n) { 
+                Console.WriteLine("Nome: " + n);
+                return n.Length;
+            };
+
             var letras = metodoDelegate("João");
             Console.WriteLine("Quantidade de letras: " + letras);
+
+            Console.ReadKey();
+
 
         }
     }
