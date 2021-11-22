03361a112ae21667536fb42f0e2cc70dee8e8693
Bruna Reveriego <Bruna.Reveriego@br.ey.com>
2021-11-22 17:44:49 -0300

    Add project files.

---
+++ Delegates/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    internal class Program
+    {
+
+        public delegate int MetodoDelegate(string Nome);
+
+        public static int Qualquer(string Pessoa)
+        {
+            Console.WriteLine("Seu Nome: " + Pessoa);
+            Console.ReadKey();
+            return Pessoa.Length;
+        }
+
+        public static void UtilizaDelegate(MetodoDelegate metodoDelegate)
+        {
+            var letras = metodoDelegate("Maria");
+            Console.WriteLine("Quantidade de letras: " + letras);
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            UtilizaDelegate(Qualquer);
+
+            MetodoDelegate metodoDelegate = Qualquer;
+            var letras = metodoDelegate("João");
+            Console.WriteLine("Quantidade de letras: " + letras);
+
+        }
+    }
+}
